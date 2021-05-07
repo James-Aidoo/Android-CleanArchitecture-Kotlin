eca9a470324ea2c36f7b22cdfed9661915f9b8db
James Eissah Aidoo <jamesaidooessah@gmail.com>
2021-05-07 17:52:30 +0000

    Upgrade gradle plugin, kotlin and gradle wrapper versions.

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -1,5 +1,5 @@
 object Kotlin {
-    const val standardLibrary = "1.4.10"
+    const val standardLibrary = "1.5.0"
     const val coroutines = "1.3.9"
 }
 
@@ -18,8 +18,8 @@ object AndroidClient {
 
 object BuildPlugins {
     object Versions {
-        const val buildToolsVersion = "4.1.0"
-        const val gradleVersion = "6.7"
+        const val buildToolsVersion = "4.2.0"
+        const val gradleVersion = "6.7.1"
         const val hilt = "2.35.1"
     }
 
